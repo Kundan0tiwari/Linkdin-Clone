cf4d8a9d35bad6de1b9c274f98c7c6e167c0af42
Kundan <kundan@Kundans-MacBook-Air.local>
2023-02-21 08:05:55 +0530

    Linkdin Clone: Skeleton Code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      Lets build Linkdin Clone
+    </div>
+  );
+}
+
+export default App;
